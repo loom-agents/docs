a66064d4ab8fd6bb645083a7bc4aebae969c064c
Chris McGravey <chris@mcgravey.dev>
2025-03-26 00:30:40 -0500

    docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://loom-agents.github.io',
-  base: 'docs',
+  base: '/docs',
   integrations: [
     starlight({
       title: "Loom",
